ce8498bf65e3b53e492fe0ace99eca7e28d299db
kaly <kalimamadou@gmail.com>
2020-05-30 02:25:04 -0700

    Created Logo component rank component imageLinkForm component and there css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from "react";
 
 import Navigation from "./components/navigation/navigation.component";
+import Logo from "./components/Logo/logo.component.jsx";
+import ImageLinkForm from "./components/ImageLinkForm/imageLinkForm.component";
+import Rank from "./components/rank/rank.component";
+import Particles from "react-particles-js";
 
 import "./App.css";
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 50,
+      density: { enable: true, value_area: 800 },
+    },
+    color: "#c01c1c",
+    polygon: {
+      nb_sides: 6,
+    },
+  },
+};
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particleOptions} />
         <Navigation />
+
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
       </div>
     );
   }
---
+++ src/components/rank/rank.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="light-red f3">{"Kaly, your current rank is..."}</div>
+      <div className="light-red f1">{"#5"}</div>
+    </div>
+  );
+};
+
+export default Rank;
